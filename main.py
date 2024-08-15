acd3c611a57050e23930dc9f7e15fea4bd181c90
mementomoryn <132916305+mementomoryn@users.noreply.github.com>
2024-08-15 21:53:09 +0800

    Only publish release apk file built

--- main.py
+++ main.py
@@ -73,10 +73,7 @@ def main():
 
     publish_release(
         [
-            f"x-piko-v{latest_version.version}.apk",
-            f"x-piko-material-you-v{latest_version.version}.apk",
             f"twitter-piko-v{latest_version.version}.apk",
-            f"twitter-piko-material-you-v{latest_version.version}.apk",
         ],
     )
 
