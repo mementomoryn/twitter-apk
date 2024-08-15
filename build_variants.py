15bb26a466a6e212418bb5bf550b863c5205326b
mementomoryn <132916305+mementomoryn@users.noreply.github.com>
2024-08-15 21:48:19 +0800

    Update build_variants.py
    - Remove unnecessary includes
    - Add patch excludes
    - Only build apk that i use

--- build_variants.py
+++ build_variants.py
@@ -9,55 +9,12 @@ def build_apks(latest_version: Version):
     patches = "bins/patches.jar"
     cli = "bins/cli.jar"
 
-    common_includes = [
-        "Enable app downgrading",
-        "Hide FAB",
-        "Disable chirp font",
-        "Add ability to copy media link",
-        "Hide Banner",
-        "Hide promote button",
-        "Hide Community Notes",
-        "Delete from database",
-        "Customize Navigation Bar items",
-        "Remove premium upsell",
-        "Control video auto scroll",
-        "Force enable translate",
-    ]
-
     patch_apk(
         cli,
         integrations,
         patches,
         apk,
-        includes=common_includes,
-        out=f"x-piko-material-you-v{latest_version.version}.apk",
-    )
-
-    patch_apk(
-        cli,
-        integrations,
-        patches,
-        apk,
-        includes=common_includes,
-        excludes=["Dynamic color"],
-        out=f"x-piko-v{latest_version.version}.apk",
-    )
-
-    patch_apk(
-        cli,
-        integrations,
-        patches,
-        apk,
-        includes=["Bring back twitter"] + common_includes,
-        out=f"twitter-piko-material-you-v{latest_version.version}.apk",
-    )
-
-    patch_apk(
-        cli,
-        integrations,
-        patches,
-        apk,
-        includes=["Bring back twitter"] + common_includes,
-        excludes=["Dynamic color"],
+        includes=["Bring back twitter"],
+        excludes=["Dynamic color", "Enable PiP mode automatically"],
         out=f"twitter-piko-v{latest_version.version}.apk",
     )
