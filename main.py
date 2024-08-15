a9273e015c902c5965eb077fa829dd590353e37b
mementomoryn <132916305+mementomoryn@users.noreply.github.com>
2024-08-16 00:32:28 +0800

    Try to use function

--- main.py
+++ main.py
@@ -57,14 +57,17 @@ def main():
     if last_integration_version is None:
         panic("Failed to fetch the latest integration version")
 
+    def previous_versions(index: int):
+        return last_build_version.body.splitline()[st_index].split(": ")[1]
+    
     # Begin stuff
     if count_releases == 0:
         print("First time building Piko Twitter!")
-    elif last_build_version.body.splitlines()[2].split(": ")[1] != latest_version.version:
+    elif previous_versions(2) != latest_version.version:
         print(f"New twitter version found: {latest_version.version}")
-    elif last_build_version.body.splitlines()[0].split(": ")[1] != last_patch_version.tag_name:
+    elif previous_versions(0) != last_patch_version.tag_name:
         print(f"New patch version found: {last_patch_version.tag_name}")
-    elif last_build_version.body.splitlines()[1].split(": ")[1] != last_integration_version.tag_name:
+    elif previous_versions(1) != last_integration_version.tag_name:
         print(f"New integration version found: {last_integration_version.tag_name}")
     else:
         print("No new version found")
