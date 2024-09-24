35ef9d6622e30167458cd9543f1f1b698e68948e
mementomoryn <132916305+mementomoryn@users.noreply.github.com>
2024-09-24 14:28:23 +0800

    Add hachidori build variants

--- build_variants.py
+++ build_variants.py
@@ -8,8 +8,10 @@ def build_apks(latest_version: Version):
     integrations = "bins/integrations.apk"
     patches = "bins/patches.jar"
     cli = "bins/cli.jar"
+    xposed = "bins/xposed.apk"
+    lspatch = "bins/lspatch.jar"
 
-    patch_apk(
+    patch_revanced_apk(
         cli,
         integrations,
         patches,
@@ -18,4 +20,11 @@ def build_apks(latest_version: Version):
         excludes=["Dynamic color", "Enable PiP mode automatically"],
         riparch=["armeabi-v7a", "x86", "x86_64"],
         out=f"twitter-piko-v{latest_version.version}.apk",
+    )
+
+    patch_xposed_apk(
+        lspatch
+        xposed
+        apk
+        out=f"twitter-hachidori-v{latest_version.version}.apk"
     )
