53e56be46537eaf99e960286d9571cda999ef9fd
mementomoryn <132916305+mementomoryn@users.noreply.github.com>
2024-10-04 16:18:24 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -90,7 +90,7 @@ def download(link, out, headers=None):
 
     # https://www.slingacademy.com/article/python-requests-module-how-to-download-files-from-urls/#Streaming_Large_Files
     with requests.get(link, stream=True, headers=headers) as r:
-        r.raise_for_status()
+        # r.raise_for_status()
         with open(out, "wb") as f:
             for chunk in r.iter_content(chunk_size=8192):
                 f.write(chunk)
