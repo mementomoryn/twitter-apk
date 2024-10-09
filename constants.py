1d0ebbb309573ff9f697aa776184aefd26275fe9
mementomoryn <132916305+mementomoryn@users.noreply.github.com>
2024-10-09 23:55:06 +0800

    Change HEADERS user agent

--- constants.py
+++ constants.py
@@ -5,7 +5,7 @@ HEADERS = {
     "cache-control": "no-cache",
     "pragma": "no-cache",
     "priority": "u=0, i",
-    "sec-ch-ua": '"Not/A)Brand";v="8", "Chromium";v="126"',
+    "sec-ch-ua": '"Not/A)Brand";v="8", "Chromium";v="129"',
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-platform": '"Windows"',
     "sec-fetch-dest": "document",
@@ -13,5 +13,5 @@ HEADERS = {
     "sec-fetch-site": "none",
     "sec-fetch-user": "?1",
     "upgrade-insecure-requests": "1",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Vivaldi/6.9.3447.51",
 }
