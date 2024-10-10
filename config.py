58f135f91224bb747d157ec87f10a96c2d0c78d4
mementomoryn <132916305+mementomoryn@users.noreply.github.com>
2024-10-10 11:11:20 +0800

    Add config variables

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 APKMIRROR_URL: str = "https://www.apkmirror.com/apk/x-corp/twitter/"
 
+APKMIRROR_CLOUDFLARE: bool = False
+
 REVANCED_PATCH_REPOSITORY: str = "crimera/piko"
 
 REVANCED_INTEGRATION_REPOSITORY: str = "crimera/revanced-integrations"
@@ -7,5 +9,11 @@ REVANCED_INTEGRATION_REPOSITORY: str = "crimera/revanced-integrations"
 REVANCED_CLI_REPOSITORY: str = "inotia00/revanced-cli"
 
 XPOSED_MODULE_REPOSITORY: str = "Xposed-Modules-Repo/com.twifucker.hachidori"
+
+LSPATCH_REPOSITORY: str = "JingMatrix/LSPatch"
+
+APKEDITOR_REPOSITORY: str = "REAndroid/APKEditor"
+
+APKRENAMER_REPOSITORY: str = "dvaoru/ApkRenamer"
 
 ENABLED_BINS: list = ["revanced", "xposed", "apkeditor"]
